d1b855685e19c4dc3e097ac7d4670c32424be9d2
Ivan-Lysov <111500735+Ivan-Lysov@users.noreply.github.com>
2022-10-26 15:20:52 +0300

    Update Practice_1.cpp

--- practice1/Practice_1/Practice_1.cpp
+++ practice1/Practice_1/Practice_1.cpp
@@ -26,7 +26,7 @@ int main()
 	BackWall = h * w * k * 800;
 	SideWall = 2 * h * d * 0.015 * 650;
 	WallUp = 2 * w * d * 0.015 * 650;
-	doors = 2 * h * w * 0.010 * 550;
+	doors =  h * w * 0.010 * 550;
 	shelves = a * d * (w - (0.015 * 2)) * 0.015 * 650;
 	printf("Total Cabinet weight %lf Kilograms\n", BackWall + SideWall + WallUp + shelves + doors);
 	return 0;
